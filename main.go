8f6b7c12ed92439ff9e8426e3601965729d43330
pikayin <pikayin@tencent.com>
2021-04-28 19:57:05 +0800

    feat:test http

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("test")
+	huoUrl := "https://api.huobi.pro/market/history/kline?period=60min&size=200&symbol=btcusdt"
+	resp, err := http.Get(huoUrl)
+	if err != nil {
+		fmt.Printf("get data from huo fail.err:%v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	rspBody, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("get data:%s", string(rspBody))
 }
